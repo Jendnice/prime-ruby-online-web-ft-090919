09c6027a5ca7e774ad2912959104da5a60bf2120
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-13 21:29:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,4 @@
 def prime?(int)
-x >= 1 
 
  if int = 1 
    true
@@ -8,33 +7,33 @@ x >= 1
  elsif 
    int <= 0 
    false 
- elsif (int % 2) != (x) || int
-   true
- else
-   false 
- 
- 
- 
-   return true
- else 
-   return false 
-
-def prime?(int)
- 
- if int < 0
+ elsif (int % 2) = 0 || int
    false
-     
-  else int == [2, 3, 7, 9, 11, 13, 17, 19]
-    true
-    
+ else
+   true 
  end
+end
+ 
+ 
+ 
+ 
+
+# def prime?(int)
+ 
+#  if int < 0
+ #   false
+     
+#   else int == [2, 3, 7, 9, 11, 13, 17, 19]
+ #    true
+    
+# end
   
-  x = 2
-  until x == 50
-    (int % x == 0)
-      x += 1
-    break if true
-        false
+#  x = 2
+#  until x == 50
+#    (int % x == 0)
+ #     x += 1
+#    break if true
+ #       false
   
   # end 
 # end
