ee3c7c5c27201bbaef62da9cba5e15e691b29324
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-13 21:40:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,4 @@
-def prime?(int)
 
- if int == 1 
-   false
- elsif int == 2 
-   true
- elsif 
-   int <= 0 
-   false 
- elsif (int % 2) == 0
-   false 
- else
-   true 
- end
-end
  
 def prime(n)
   return false if n < 2
