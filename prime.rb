8560f5932f5ceb4c1ece909a0b5a65b4d16dab9c
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-13 21:54:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -12,29 +12,4 @@ end
 # end
  
  
- 
-
-# def prime?(int)
- 
-#  if int < 0
- #   false
-     
-#   else int == [2, 3, 7, 9, 11, 13, 17, 19]
- #    true
-    
-# end
-  
-#  x = 2
-#  until x == 50
-#    (int % x == 0)
- #     x += 1
-#    break if true
- #       false
-  
-  # end 
-# end
-
-# prime number = true 
-# composite number = false
-
 
