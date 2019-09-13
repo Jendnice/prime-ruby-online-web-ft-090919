2201e502b3f1b9393f0fb95f552db05fe27b2d21
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-13 21:14:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+prime?(i)
+numbers = [2..n]
+p = 2 
+new_numbers = [2p..n]
+ if i 
+   return true
+ else 
+   return false 
+
+
+
+# prime number = true 
+# composite number = false
+
+To find all the prime numbers less than or equal to a given integer n by Eratosthenes' method:
+
