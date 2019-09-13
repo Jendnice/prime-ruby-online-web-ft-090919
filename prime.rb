78a0f83a0e9f3a0ca1e7263c1265205de24e552e
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-13 21:48:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,11 @@ def prime?(int)
 end
  
  
-def prime?(n)
-  return false if n < 2
+# def prime?(n)
+#  return false if n < 2
 
-  (2..n/2).none?{|i| n % i == 0}
-end
+#  (2..n/2).none?{|i| n % i == 0}
+# end
  
  
  
