4d5015259f2802a5feb20629a16cc69de2627e39
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-13 21:24:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,45 @@
-prime?(i)
-numbers = [2..n]
-p = 2 
-new_numbers = [2p..n]
- if i 
+def prime?(int)
+x >= 1 
+
+ if int = 1 
+   true
+ elsif int = 2 
+   true
+ elsif 
+   int <= 0 
+   false 
+ elsif (int % 2) != (x) || int
+   true
+ else
+   false 
+ 
+ 
+ 
    return true
  else 
    return false 
 
-
+def prime?(int)
+ 
+ if int < 0
+   false
+     
+  else int == [2, 3, 7, 9, 11, 13, 17, 19]
+    true
+    
+ end
+  
+  x = 2
+  until x == 50
+    (int % x == 0)
+      x += 1
+    break if true
+        false
+  
+  # end 
+# end
 
 # prime number = true 
 # composite number = false
 
-To find all the prime numbers less than or equal to a given integer n by Eratosthenes' method:
 
